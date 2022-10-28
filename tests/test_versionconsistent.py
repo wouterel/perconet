4f17227733d3f6bb3c0722e9af5675d97e201c8d
Wouter Ellenbroek <w.g.ellenbroek@tue.nl>
2022-10-28 20:04:10 +0200

    Added pytest for checking __version__ is set correctly

---
+++ tests/test_versionconsistent.py
@@ -0,0 +1,24 @@
+# This file is part of the perconet package
+# (c) 2022 Eindhoven University of Technology
+# Released under EUPL v1.2
+# See LICENSE file for details
+# Contributors:
+# * Chiara Raffaelli
+# * Wouter G. Ellenbroek
+# Contributing: https://github.com/wouterel/perconet
+
+import perconet as pn
+# import pytest
+import toml
+import os
+
+
+def test_version():
+    testdir = os.path.dirname(os.path.abspath(__file__))
+    tomlfile = os.path.join(testdir, "..", "pyproject.toml")
+    release = toml.load(tomlfile)['tool']['poetry']['version']
+    assert release == pn.__version__
+
+
+if __name__ == "__main__":
+    test_version()
